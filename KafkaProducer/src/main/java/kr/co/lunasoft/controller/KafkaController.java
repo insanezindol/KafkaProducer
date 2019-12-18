0d87d9a88d6457d72f562c1b677705762506c352
딘 <jhlee@lunasoft.co.kr>
2019-12-18 14:11:28 +0900

    dev kafka producer

--- KafkaProducer/src/main/java/kr/co/lunasoft/controller/KafkaController.java
+++ KafkaProducer/src/main/java/kr/co/lunasoft/controller/KafkaController.java
@@ -4,30 +4,39 @@ import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.google.gson.Gson;
+
+import kr.co.lunasoft.model.MessageInfo;
 import kr.co.lunasoft.service.KafkaService;
+import kr.co.lunasoft.util.DateUtil;
 import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @RequestMapping("/kafka")
 @Slf4j
 public class KafkaController {
-	
+
 	@Autowired
-    private KafkaService kafkaService;
-	
-	@GetMapping(value = "/send/{topic}/{message}")
-    public JSONObject send(@PathVariable String topic, @PathVariable String message) {
-    	kafkaService.send(topic, message);
-    	
-        JSONObject obj = new JSONObject();
-        obj.put("code", "100200");
-        obj.put("msg", "success");
-        obj.put("data", null);
-        return obj;
-    }
-	
+	private KafkaService kafkaService;
+
+	@GetMapping(value = "/send/{topic}")
+	public JSONObject send(@PathVariable String topic, @RequestBody MessageInfo messageInfo) {
+		messageInfo.setCreated(DateUtil.getNowDatetime());
+
+		Gson gson = new Gson();
+		String message = gson.toJson(messageInfo);
+
+		kafkaService.send(topic, message);
+
+		JSONObject obj = new JSONObject();
+		obj.put("code", "100200");
+		obj.put("msg", "success");
+		obj.put("data", null);
+		return obj;
+	}
 
 }
---
+++ KafkaProducer/src/main/java/kr/co/lunasoft/model/MessageInfo.java
@@ -0,0 +1,11 @@
+package kr.co.lunasoft.model;
+
+import lombok.Data;
+
+@Data
+public class MessageInfo {
+	private String seq;
+	private String name;
+	private String desc;
+	private String created;
+}
