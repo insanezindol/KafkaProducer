3e383274156f22757a4202b346fc2600b2c96ce2
딘 <jhlee@lunasoft.co.kr>
2019-12-18 10:52:01 +0900

    init

---
+++ KafkaProducer/src/main/java/kr/co/lunasoft/service/KafkaService.java
@@ -0,0 +1,21 @@
+package kr.co.lunasoft.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@Slf4j
+public class KafkaService {
+
+	@Autowired
+	private KafkaTemplate<String, String> kafkaTemplate;
+
+	public void send(String topic, String message) {
+		log.info(topic + " : " + message);
+		kafkaTemplate.send(topic, message);
+	}
+
+}
